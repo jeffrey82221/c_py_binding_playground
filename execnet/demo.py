0e3588669344f58f45ca745ef49b675ba7bd99f6
jeffrey82221 <jeffrey82221@gmail.com>
2022-12-11 23:47:43 +0800

    feature: add execnet demo

---
+++ execnet/demo.py
@@ -0,0 +1,16 @@
+import execnet
+gw = execnet.makegateway("popen//pypy")
+channel = gw.remote_exec("""
+    array = []
+    while 1:
+        x = channel.receive()
+        if x is None:
+            break
+        array.append(x)
+    channel.send(repr(array))
+""")
+for x in range(10):
+    channel.send([x])
+channel.send(None)
+ans = eval(channel.receive())
+print(ans)
